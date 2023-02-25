e5391b6b4721e42913eb60258dec61ec335864b3
SurajRoy0 <myself.suraj0@gmail.com>
2023-02-26 01:39:27 +0530

    JsPractice3

---
+++ main.js
@@ -0,0 +1,6 @@
+document.querySelector('title');
+const headerTitle = document.querySelector('#header-title');
+headerTitle.style.borderBottom = '3px solid #000';
+const Title = document.querySelector('.title');
+Title.style.fontWeight = "bold";
+Title.style.color = "green";
