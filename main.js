0ef56516014911458efe3cda86ea9eb0eda7c77a
SurajRoy0 <myself.suraj0@gmail.com>
2023-02-26 02:04:14 +0530

    JS-Practice-3-1

--- main.js
+++ main.js
@@ -4,3 +4,10 @@ headerTitle.style.borderBottom = '3px solid #000';
 const Title = document.querySelector('.title');
 Title.style.fontWeight = "bold";
 Title.style.color = "green";
+
+const items = document.querySelectorAll('.list-group-item');
+items[2].style.color='green';
+
+for(let i=0;i<items.length;i++){
+   items[i].style.fontWeight='bold';
+}
