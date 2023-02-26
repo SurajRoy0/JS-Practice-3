451a0076a622705ed553d245f59a3fb045c7cd1a
SurajRoy0 <myself.suraj0@gmail.com>
2023-02-26 12:03:34 +0530

    first commit

--- main.js
+++ main.js
@@ -1,13 +1,9 @@
-document.querySelector('title');
-const headerTitle = document.querySelector('#header-title');
-headerTitle.style.borderBottom = '3px solid #000';
-const Title = document.querySelector('.title');
-Title.style.fontWeight = "bold";
-Title.style.color = "green";
+var items = document.querySelectorAll('li');
+items[1].style.color='green'
+// items[2].style.visibility='hidden';
 
-const items = document.querySelectorAll('.list-group-item');
-items[2].style.color='green';
-
+var items = document.querySelectorAll('li:nth-child(odd)');
 for(let i=0;i<items.length;i++){
-   items[i].style.fontWeight='bold';
+   items[i].style.backgroundColor='green';
+
 }
